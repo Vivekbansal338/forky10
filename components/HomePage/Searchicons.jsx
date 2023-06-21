f73ab05573b99ebc9f9af06d6ab0d5aecc914e5e
Vivekbansal338 <Vivekb379@gmail.com>
2023-06-22 04:16:20 +0530

    Added new files

--- components/HomePage/Searchicons.jsx
+++ components/HomePage/Searchicons.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Searchicons.css";
+import Image from "next/image";
 
 function SearchIcons(props) {
   const { name, icon_url } = props;
   return (
     <div className="Searchicons">
       <a href="#">
-        <img src="/artichoke.png" alt={name} />
+        <Image
+          src="/assets/Vegetables/carrot.png"
+          width="200"
+          height="200"
+          alt={name}
+        />
         <img src="/favicon.ico" alt={name} />
         <div className="name">{name}</div>
       </a>
